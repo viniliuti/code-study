7552d9c27f917eb1564560fcd9a5d17e675af180
Vinicio Liuti <viniliuti@live.com>
2024-05-04 20:03:31 -0300

    implementing recipe API with serializer and overriding get_queryset

---
+++ python/python-django/app/recipe/urls.py
@@ -0,0 +1,14 @@
+"""URL mappings for the recipe app"""
+
+from django.urls import include, path
+from recipe import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("recipes", views.RecipeViewSet)
+
+app_name = "recipe"
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
