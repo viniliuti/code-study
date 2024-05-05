c2920d547d3527cef564cb6fc5b779d4071e9314
Vinicio Liuti <viniliuti@live.com>
2024-05-05 19:01:51 -0300

    refactor serializers for update work with nested tags

--- python/python-django/app/recipe/serializers.py
+++ python/python-django/app/recipe/serializers.py
@@ -30,21 +30,49 @@ class RecipeSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ["id"]
 
+    def _get_context_user(self):
+        """Get authenticated user from context"""
+        return self.context["request"].user
+
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tags as needed"""
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=self._get_context_user(),
+                **tag,
+            )
+
+            recipe.tags.add(tag_obj)
+
     def create(self, validated_data):
         """Create a recipe"""
         tags = validated_data.pop("tags", [])
 
         recipe = Recipe.objects.create(**validated_data)
-        auth_user = self.context["request"].user
-
-        for tag in tags:
-            tag_obj, created = Tag.objects.get_or_create(
-                user=auth_user,
-                **tag,
-            )
-            recipe.tags.add(tag_obj)
+        self._get_or_create_tags(
+            tags,
+            recipe,
+        )
 
         return recipe
+
+    def update(self, instance, validated_data):
+        """Update recipe"""
+        tags = validated_data.pop("tags", None)
+
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(
+                tags,
+                instance,
+            )
+
+        # fills all other values back again (? why )
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
--- python/python-django/app/recipe/tests/test_recipe_api.py
+++ python/python-django/app/recipe/tests/test_recipe_api.py
@@ -285,3 +285,57 @@ class PrivateRecipeAPITests(TestCase):
                     user=self.user,
                 ).exists()
             )
+
+    def test_create_tag_on_update(self):
+        """Test creating tag when updating a recipe"""
+        recipe = create_recipe(user=self.user)
+
+        payload = {"tags": [{"name": "Lunch"}]}
+        url = detail_url(recipe.id)
+
+        res = self.client.patch(url, payload, format="json")
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        new_tag = Tag.objects.get(user=self.user, name="Lunch")
+        self.assertIn(new_tag, recipe.tags.all())
+
+    def test_update_recipe_assign_tag(self):
+        """Test assigning an existing tag when updating a recipe"""
+        # don't understand this logic very much
+        # very weird ORM stuff happening for my little brain
+
+        tag_breakfast = Tag.objects.create(
+            user=self.user,
+            name="Breakfast",
+        )
+        recipe = create_recipe(user=self.user)
+        recipe.tags.add(tag_breakfast)
+
+        tag_lunch = Tag.objects.create(
+            user=self.user,
+            name="Lunch",
+        )
+        payload = {"tags": [{"name": "Lunch"}]}
+
+        url = detail_url(recipe.id)
+        res = self.client.patch(url, payload, format="json")
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        recipes = recipe.tags.all()
+        self.assertIn(tag_lunch, recipes)
+        self.assertNotIn(tag_breakfast, recipes)
+
+    def test_clear_recipe_tags(self):
+        """Test clearing a recipes tags"""
+        tag = Tag.objects.create(user=self.user, name="Dessert")
+        recipe = create_recipe(user=self.user)
+        recipe.tags.add(tag)
+
+        payload = {"tags": []}
+        url = detail_url(recipe.id)
+        res = self.client.patch(url, payload, format="json")
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(recipe.tags.count(), 0)
