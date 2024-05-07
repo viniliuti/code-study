77104c331562a0261f59da9bd1aa1757dd09fa90
Vinicio Liuti <viniliuti@live.com>
2024-05-06 21:43:47 -0300

    refactor views and fix URLs

--- python/python-django/app/app/urls.py
+++ python/python-django/app/app/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
         name="api-docs",
     ),
     path("api/user/", include("user.urls")),
-    path("api/recipe", include("recipe.urls")),
+    path("api/recipe/", include("recipe.urls")),
 ]
