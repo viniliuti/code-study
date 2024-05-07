42ec3ca0f3c78c076ebc7c3eacab622543ac6284
Vinicio Liuti <viniliuti@live.com>
2024-05-06 23:16:42 -0300

    implement upload image and recipe-detail to show full url

--- python/python-django/app/recipe/views.py
+++ python/python-django/app/recipe/views.py
@@ -2,9 +2,11 @@
 
 from core.models import Ingredient, Recipe, Tag
 from recipe import serializers
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, status, viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -27,12 +29,35 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return (
                 serializers.RecipeSerializer
             )  # do not return RecipeSerializer(), it wants reference of a class
+        elif self.action == "upload_image":
+            return serializers.RecipeImageSerializer
 
         return self.serializer_class
 
     def perform_create(self, serializer):
         """Create a new recipe"""  # overrides Django
         serializer.save(user=self.request.user)
+
+    @action(methods=["POST"], detail=True, url_path="upload-image")
+    def upload_image(self, request, pk=None):
+        """Upload an image to recipe"""
+        recipe = self.get_object()
+        serializer = self.get_serializer(
+            recipe,
+            data=request.data,
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK,
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )
 
 
 class BaseRecipeAttrViewSet(
