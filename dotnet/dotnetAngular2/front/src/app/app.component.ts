bdab73a170882ccb6a71cc70f025ff001b10ad39
Vinicio Liuti <viniliuti@live.com>
2025-02-21 19:47:29 -0300

    ng update angular v19

--- dotnet/dotnetAngular2/front/src/app/app.component.ts
+++ dotnet/dotnetAngular2/front/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   title = 'dotnetAngular';
--- dotnet/dotnetAngular2/front/src/app/car/car.component.ts
+++ dotnet/dotnetAngular2/front/src/app/car/car.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-car',
-  templateUrl: './car.component.html',
-  styleUrls: ['./car.component.scss']
+    selector: 'app-car',
+    templateUrl: './car.component.html',
+    styleUrls: ['./car.component.scss'],
+    standalone: false
 })
 export class CarComponent {
 
